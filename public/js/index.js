19ff0719cdf83ae18e02b66a33ae238853d9f581
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-11-06 16:46:33 -0600

    made dedicated js

---
+++ public/js/index.js
@@ -0,0 +1,114 @@
+
+function getPieces() {
+  $.get("/api/gameStart", function(data) {
+      console.log(data);
+      var pieces = [];
+      for (var i = 0; i < data.length; i++) {                
+          // console.log(data[i].id);
+          id = data[i].id;
+
+          // console.log(data[i].cx);
+          currcx = data[i].cx;
+
+          // console.log(data[i].cy);
+          currcy = data[i].cy;
+          
+          $("#"+id).attr({cx: currcx, cy: currcy});
+      }
+  });
+}
+getPieces();
+
+  
+  
+      
+  // $(document).on("click", ".node", movePieceOne);
+  // function movePieceOne(event) {
+  //     event.stopPropagation();
+  //     var newX = $(this).attr("cx"); 
+  //     var newY = $(this).attr("cy"); 
+  //     console.log(newX, newY); 
+  //     $("#1").attr({cx: newX, cy: newY});       
+  // }
+
+
+$("#1").on("click", function(event) {
+    event.stopPropagation();
+    var id = $(this).attr("id");
+  $(".node").on("click", function(event) {
+    let newX = $(this).attr("cx");
+    let newY = $(this).attr("cy");
+    $("#1").attr({cx: newX, cy: newY});
+    $.ajax({
+          method: "PUT",
+          url: "/api/piece/" + id,
+          data: {
+              cy: newY,
+              cx: newX 
+          }
+    }).then(getPieces);
+  });
+});
+
+$("#2").on("click", function(event) {
+  event.stopPropagation();
+  var id = $(this).attr("id");
+  $(".node").on("click", function() {
+      
+      let newX = $(this).attr("cx");
+      let newY = $(this).attr("cy");
+      $("#2").attr({cx: newX, cy: newY});
+      
+      // console.log(newX, newY);
+      // console.log(id);
+      $.ajax({
+          method: "PUT",
+          url: "/api/piece/" + id,
+          data: {
+              cy: newY,
+              cx: newX 
+          }
+      }).then(getPieces);
+});
+});
+
+$("#3").on("click", function() {
+  event.stopPropagation();
+  var id = $(this).attr("id");
+$(".node").on("click", function() {
+  
+  let newX = $(this).attr("cx");
+  let newY = $(this).attr("cy");
+
+  $("#3").attr({cx: newX, cy: newY});
+  $.ajax({
+      method: "PUT",
+      url: "/api/piece/" + id,
+      data: {
+          cy: newY,
+          cx: newX 
+      }
+  }).then(getPieces);
+});
+});
+
+// New code!!
+$("#4").on("click", function() {
+  event.stopPropagation();
+  var id = $(this).attr("id");
+$(".node").on("click", function() {
+  
+  let newX = $(this).attr("cx");
+  let newY = $(this).attr("cy");
+
+  $("#4").attr({cx: newX, cy: newY});
+  $.ajax({
+      method: "PUT",
+      url: "/api/piece/" + id,
+      data: {
+          cy: newY,
+          cx: newX 
+      }
+  }).then(getPieces);
+});
+});
