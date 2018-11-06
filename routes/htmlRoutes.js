a7bcc5cae131baca143981c58343c2cfadcd037d
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-11-06 16:05:37 -0600

    Fixed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = app => {
   // Load index page
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/test.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   // app.get("/", (req, res) => {
